1a3fa5c4d51635828ba9af212c9b4a9a6b945e3a
camposouza <guilhermesouza0911@outlook.com>
2026-08-29 16:15:20 -0300

    Stylistic change

--- rtl/hazard_detection_unit.vhd
+++ rtl/hazard_detection_unit.vhd
@@ -29,8 +29,8 @@ begin
     pc_write <= not load_use_hazard;
     if_id_write <= not load_use_hazard;
 
-    -- Branches are predicted not taken and resolved in EX. On a taken
-    -- branch, both younger instructions must be discarded.
+    -- Branches are predicted not taken and resolved in EX.
+    -- On a taken   branch, both younger instructions must be discarded.
     if_id_flush <= branch_taken;
     id_ex_flush <= load_use_hazard or branch_taken;
 end architecture;
